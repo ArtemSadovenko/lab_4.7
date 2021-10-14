dc7f307397836ac4b583597a6d3387861446d1d6
ArtemSadovenko <90398799+ArtemSadovenko@users.noreply.github.com>
2021-10-14 23:12:04 +0300

    Add files via upload

---
+++ lab_4.7/lab_4.7/Source.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	double r, a, dx , xp, xk, x, eps, s = 0;
+	int n;
+
+	/*cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;*/
+
+	xp = 0;
+	xk = 2;
+	dx = 0.01;
+	eps = 0.00001;
+
+	cout << fixed;         
+	cout << "-------------------------------------------------" << endl;         
+	cout << "|" << setw(5) << "x" << "     |" << setw(10) << "exp(x)" << "   |" << setw(7) << "S" << "      |" << setw(5) << "n" << "   |" << endl;         cout << "-------------------------------------------------" << endl;
+
+
+	x = xp;
+
+	while (x <= xk) {
+		n = 0;
+		a = x - 1;
+		s = a;
+		do {
+			n++;
+			r = -(n * (x - 1.0)) / (n + 1.0);
+			a *= r;
+			s += a;
+		} while (abs(a) >= eps);
+
+		cout << "|" << setw(7) << setprecision(2) << x << "   |" << setw(10) << setprecision(5) << exp(x) << "   |" << setw(10) << setprecision(5) << s << "   |" << setw(5) << n << "   |" << endl << "-------------------------------------------------" << endl;;
+		x += dx;
+	}
+
+
+
+	return 0;
+}
